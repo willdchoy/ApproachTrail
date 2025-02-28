6e2a11fdbf05f2be622779263e624c3b6b966ea3
William Choy <williamchoy@Williams-MacBook-Pro.local>
2025-02-28 15:25:15 -0500

    Refactor prouduct list by media query.

--- client/src/components/ProductSlider/ProductSlider.tsx
+++ client/src/components/ProductSlider/ProductSlider.tsx
@@ -1,25 +1,20 @@
 import "./ProductSlider.css";
 import { Product } from "@/types/types";
 import { getCategoryFromDB } from "@/db/db.js";
-import ProductSlide from "@/components/ProductSlide/ProductSlide";
 
 function ProductSlider() {
-  const category = "womens-jackets";
+  const category = "mens-jackets";
   const mensJackets = getCategoryFromDB(category);
 
   return (
-    <div className="product-slider">
-      <div className="breadcrumbs">
-        <h2>{mensJackets.metadata.categoryName}</h2>
-        <span>&gt; The North Face</span>
-        <span>&gt; Rain</span>
-      </div>
-
-      <div className="products">
-        {mensJackets.products.map((product: Product) => {
-          return <ProductSlide key={product.id} {...product} />;
-        })}
-      </div>
+    <div className="product-list dev-border">
+      {mensJackets.products.map((product: Product, index: number) => {
+        return (
+          <div key={index} className="product">
+            {product.title}
+          </div>
+        );
+      })}
     </div>
   );
 }
