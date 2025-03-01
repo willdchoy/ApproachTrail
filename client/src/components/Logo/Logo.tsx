daa320cfc861b18e6e5c4ff6dc5c6618c03ce92c
William Choy <williamchoy@Williams-MacBook-Pro.local>
2025-03-01 12:45:17 -0500

    Sitewide css updates. Add new font for logo.

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import "./Logo.css";
 function Logo() {
   return (
     <div className="logo header-grid-logo">
-      <h1 aria-label="Approach Trail">ApproachTrail</h1>
+      <h1>ApproachTrail</h1>
     </div>
   );
 }
