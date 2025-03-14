816f22d1d1aeb6b0c504348a500d4a945a86cace
William Choy <williamchoy@Williams-MacBook-Pro.local>
2025-03-13 20:19:03 -0400

    Refactor business logic in routes to new services directory.

--- api/routes/products/products:id/productId.js
+++ api/routes/products/products:id/productId.js
@@ -1,46 +1,12 @@
+import { generateProductById } from "../../../services/product-service.js";
+
 export default async function (fastify) {
   fastify.get("/products/:id", async (req, reply) => {
-    const client = await fastify.pg.connect();
-
     try {
-      const { rows: productData } = await client.query(
-        `
-        SELECT pi.product_item_id, pi.sku, pi.qty_in_stock, p.name, p.description, pb.brand_code, pb.brand_name
-        FROM product_item as pi
-        JOIN product as p
-        ON p.product_id = pi.product_id
-        JOIN product_brand as pb
-        ON p.brand_id = pb.product_brand_id
-        WHERE pi.product_item_id = $1
-        `,
-        [req.params.id]
+      const productResponseObj = await generateProductById(
+        fastify,
+        req.params.id
       );
-
-      const { rows: attributeData } = await client.query(
-        `
-        SELECT attribute_code, display_name, value
-        FROM product_attribute as pa
-        JOIN product_attribute_value_varchar as pavc
-        ON pavc.product_attribute_id = pa.product_attribute_id
-        WHERE pa.product_item_id = $1
-        `,
-        [req.params.id]
-      );
-
-      console.log(attributeData);
-
-      const productResponseObj = {
-        ...productData[0],
-        attributes: {
-          sizes: attributeData.filter(
-            (attribute) => attribute.attribute_code === "size"
-          ),
-          colors: attributeData.filter(
-            (attribute) => attribute.attribute_code === "color"
-          ),
-        },
-      };
-
       reply.send(productResponseObj);
     } catch (e) {
       console.log("Error", e);
