ae2a0c44476d8c5a6867cb3aa1e6fe032ddeef4f
William Choy <williamchoy@Williams-MacBook-Pro.local>
2025-03-17 10:32:22 -0400

    Init commit for spider. Basic test on zpacks site.

---
+++ spider/scripts/zpacks.js
@@ -0,0 +1,43 @@
+const playwright = require("playwright");
+
+(async () => {
+  const url = "https://zpacks.com/collections/mens-clothing";
+  const productClass = ".product.product--portrait";
+  let browser;
+
+  try {
+    for (const browserType of ["chromium"]) {
+      browser = await playwright[browserType].launch({
+        logger: {
+          isEnabled: (name, severity) => true,
+          log: (name, severity, message, args) =>
+            console.log(`${name} ${severity} ${message}`),
+        },
+      });
+      const context = await browser.newContext();
+      const page = await context.newPage();
+
+      await page.goto(url);
+      await page.waitForTimeout(1000);
+
+      const products = await page.$$eval(productClass, (all_products) => {
+        console.log(all_products);
+        const data = [];
+        all_products.forEach((product) => {
+          const titleEl = product.querySelector(".product__title > a");
+          const title = titleEl ? titleEl.innerText : null;
+          const priceEl = product.querySelector("span.money");
+          const price = priceEl ? priceEl.innerText : null;
+          data.push({ title, price });
+        });
+        return data;
+      });
+      console.log(products);
+
+      await browser.close();
+    }
+  } catch (e) {
+    console.log(e);
+    await browser.close();
+  }
+})();
