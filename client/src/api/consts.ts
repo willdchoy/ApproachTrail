360d383fe772fb84969487deb094c51a46b91d37
William Choy <williamchoy@Williams-MacBook-Pro.local>
2025-03-24 19:16:46 -0400

    Added Tanstack Query.

---
+++ client/src/api/consts.ts
@@ -0,0 +1,4 @@
+export const VERSION = "api/v1";
+export const PORT = 3000;
+export const DOMAIN = "http://192.168.1.69";
+export const API_BASE_URL = `${DOMAIN}:${PORT}/${VERSION}`;
--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,91 +1,19 @@
 import "./ProductList.css";
+import { useQuery } from "@tanstack/react-query";
 import { Product as TProduct } from "@/types/types";
 import ProductCard from "@/components/ProductCard/ProductCard";
-import { useService } from "@/hooks/useService";
+import { getProductsByCategory } from "@/api/products";
+
+const categoryId = 1;
 
 function ProductList() {
-  const { isLoading, error, data } = useService("/categories/2");
-  const products = data as TProduct[];
-
-  console.log(isLoading, error, data);
+  const { data: products } = useQuery({
+    queryKey: ["categoryId", categoryId],
+    queryFn: () => getProductsByCategory(categoryId),
+  });
 
   return (
     <div className="product-list">
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
-      {products
-        ? products.map((product: TProduct) => {
-            return <ProductCard key={Math.random()} {...product} />;
-          })
-        : "No products available!"}
       {products
         ? products.map((product: TProduct) => {
             return <ProductCard key={Math.random()} {...product} />;
